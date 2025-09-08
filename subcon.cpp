8615ad83d74d22982de74dca8c6d99589585ca6c
3Inaba <cradgonzalezgtz@gmail.com>
2025-09-07 21:38:14 -0600

    viruenyada 3

--- subcon.cpp
+++ subcon.cpp
@@ -1,6 +1,25 @@
 #include <iostream>
 #include <cstdlib>
 
+void Combinar(int ini, int res, int *alfabeto, int n, int* conjunto, int tam);
+
+int main(int argn, char **args)
+{
+	int card = atoi(args[1]);
+	int *inte = new int [card];
+	int *conjunto = new int [card];
+
+	for(int k=0; k<card; k++)
+		inte[k]=k+1;
+
+	for(int k=0; k<=card; k++)
+	{
+		Combinar(0, k, inte, card, conjunto, 0);
+	}
+
+	return 0;
+}
+
 void Combinar(int ini, int res, int *alfabeto, int n, int* conjunto, int tam)
 {
 	if (res==0)
@@ -22,21 +41,4 @@ void Combinar(int ini, int res, int *alfabeto, int n, int* conjunto, int tam)
 		conjunto[tam]=alfabeto[k];
 		Combinar(k+1, res-1, alfabeto, n, conjunto, tam+1);
 	}
-}
-
-int main(int argn, char **args)
-{
-	int card = atoi(args[1]);
-	int *inte = new int [card];
-	int *conjunto = new int [card];
-
-	for(int k=0; k<card; k++)
-		inte[k]=k+1;
-
-	for(int k=0; k<=card; k++)
-	{
-		Combinar(0, k, inte, card, conjunto, 0);
-	}
-
-	return 0;
 }
